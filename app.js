c3e1e6d399041278f9aab615bb051b128f25be1c
EthanWatkins1720 <70484753+EthanWatkins1720@users.noreply.github.com>
2023-11-02 00:24:02 -0400

    Altered Hello message

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const port = process.env.PORT || 3001;
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("Hello World! Did you know a group of jellyfish is called a smack of jellyfish? If that's not fun enough, did you know \"You've Lost That Lovin' Feeling\" by The Righteous Brothers was the most played song on the radio in the 20th century! Do you know what movie caused that?");
+    res.end("Hello World! Did you know a group of jellyfish is called a smack of jellyfish? If that's not fun enough, did you know \"You've Lost That Lovin' Feelin'\" by The Righteous Brothers was the most played song on the radio in the 20th century! Do you know what movie caused that?");
   })
   .listen(port);
